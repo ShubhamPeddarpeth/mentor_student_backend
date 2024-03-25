bb26ad01ff8d39ce73ed5409d905fadfd64495d3
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-03-25 17:32:18 +0530

    code updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const mentorRouter = require("./Routes/MentorRouter");
 const studentRouter = require("./Routes/StudentRouter");
-require('./db/connection')
+require("./db/connection");
 const express = require("express");
 const app = express();
 
@@ -13,22 +13,19 @@ app.use(cors()); /* To avoid cross origin error */
 app.use(express.json());
 
 const PORT = process.env.PORT || 4100;
- 
-
-
 
 app.get("/", (req, res) =>
   res.send(`
 <div>
 <p> In Home Page </p>
-<p>To get all mentor List - https://zen-assign-mentors.herokuapp.com/Mentors </p>
+<p>To get all mentor List - https://mentor-student-backend-d58b.onrender.com/Mentors </p>
 <br>
-<p>To get all Students List - https://zen-assign-mentors.herokuapp.com/Students </p>
+<p>To get all Students List - https://mentor-student-backend-d58b.onrender.com/Students </p>
 <br>
-<p>To get mentor based on ID - https://zen-assign-mentors.herokuapp.com/Mentors/get-mentor/:id<p>
-<p>sample - https://zen-assign-mentors.herokuapp.com/Mentors/get-mentor/60e7f515d5ff5342a06652e3 </p>
+<p>To get mentor based on ID - https://mentor-student-backend-d58b.onrender.com//Mentors/get-mentor/:id<p>
+<p>sample - https://mentor-student-backend-d58b.onrender.com//Mentors/get-mentor/60e7f515d5ff5342a06652e3 </p>
 
-<p> To test Post and update - visit Frontend page of the application - https://preethi-st.github.io/ZEN-Mentors-Frontend/ </p>
+<p> To test Post and update - visit Frontend page of the application - https://shubhampeddarpeth.github.io/mentor_student_frontend/ </p>
 </div>
 `)
 );
