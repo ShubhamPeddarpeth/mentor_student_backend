4cc73481f6fdf32ae4a16dfbf46dac356e4ad029
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-03-26 16:38:05 +0530

    updated

--- index.js
+++ index.js
@@ -15,22 +15,6 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 4100;
 
-app.get("/", (req, res) =>
-  res.send(`
-<div>
-<p> In Home Page </p>
-<p>To get all mentor List - https://mentor-student-backend-ksz8.onrender.com/Mentors </p>
-<br>
-<p>To get all Students List - https://mentor-student-backend-ksz8.onrender.com/Students </p>
-<br>
-<p>To get mentor based on ID - https://mentor-student-backend-ksz8.onrender.com/Mentors/get-mentor/:id<p>
-<p>sample - https://mentor-student-backend-ksz8.onrender.com/Mentors/get-mentor/60e7f515d5ff5342a06652e3 </p>
-
-<p> To test Post and update - visit Frontend page of the application - https://shubhampeddarpeth.github.io/mentor_student_frontend/ </p>
-</div>
-`)
-);
-
 app.use("/Mentors", mentorRouter);
 app.use("/Students", studentRouter);
 
