2cbcfbe01e865567169ec014927add85cd2e0847
ShubhamPeddarpeth <shubhampeddarpeth@gmail.com>
2024-03-27 12:37:58 +0530

    cors

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ const studentRouter = require("./Routes/StudentRouter");
 require("./db/connection");
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const dotenv = require("dotenv");
 dotenv.config();
-
-const cors = require("cors");
-app.use(cors()); /* To avoid cross origin error */
+const corsOptions = {
+  origin: "http://frontend-domain.com", // Replace with your frontend domain
+  methods: ["GET", "POST", "PATCH", "DELETE"], // Allow only specific HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allow only specific headers
+};
+app.use(cors(corsOptions)); /* To avoid cross origin error */
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,7 +25,7 @@ app.use("/Students", studentRouter);
 // Error handler middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).send("Something went wrong!");
 });
 
 app.listen(PORT, () => console.log(`Server started in the port ${PORT}`));
